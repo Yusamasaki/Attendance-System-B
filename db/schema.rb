53a7fcf3bc71dcd237f29b91c0148cadd6252168
EC2 Default User <ec2-user@ip-172-31-45-183.ap-northeast-1.compute.internal>
2022-01-04 12:36:39 +0000

    gem pg を削除

--- db/schema.rb
+++ db/schema.rb
@@ -65,8 +65,8 @@ ActiveRecord::Schema.define(version: 20200229193421) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "designated_work_start_time", default: "2020-09-05 01:00:00"
-    t.datetime "designated_work_end_time", default: "2020-09-05 10:00:00"
+    t.datetime "designated_work_start_time", default: "2022-01-04 01:00:00"
+    t.datetime "designated_work_end_time", default: "2022-01-04 10:00:00"
     t.datetime "expected_end_time"
     t.string "next_day"
     t.string "business_processing_contents"
@@ -94,8 +94,8 @@ ActiveRecord::Schema.define(version: 20200229193421) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "designated_work_start_time", default: "2020-09-05 01:00:00"
-    t.datetime "designated_work_end_time", default: "2020-09-05 10:00:00"
+    t.datetime "designated_work_start_time", default: "2022-01-04 01:00:00"
+    t.datetime "designated_work_end_time", default: "2022-01-04 10:00:00"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
@@ -103,8 +103,8 @@ ActiveRecord::Schema.define(version: 20200229193421) do
     t.boolean "admin", default: false
     t.string "department"
     t.string "affiliation"
-    t.datetime "basic_time", default: "2020-09-04 23:00:00"
-    t.datetime "work_time", default: "2020-09-04 22:30:00"
+    t.datetime "basic_time", default: "2022-01-03 23:00:00"
+    t.datetime "work_time", default: "2022-01-03 22:30:00"
     t.boolean "superior", default: false
     t.boolean "superiorA", default: false
     t.boolean "superiorB", default: false
